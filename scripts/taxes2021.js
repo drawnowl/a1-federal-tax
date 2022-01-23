c4b54875feb8c4e12c07dc85bcb96b84f077945e
drawnowl <drawnowl@gmail.com>
2022-01-23 09:01:05 +0200

    denomination fix

--- scripts/taxes2021.js
+++ scripts/taxes2021.js
@@ -1,7 +1,7 @@
 const singlePersonTax = {
   rates:    [0.10, 0.22,  0.24,  0.32,   0.35,   0.37],
   brackets: [9950, 40525, 86375, 164925, 523600, Number.MAX_SAFE_INTEGER],
-  denomination: 12250
+  denomination: 12550
 }
 
 export { singlePersonTax };
